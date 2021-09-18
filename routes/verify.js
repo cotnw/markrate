b0f442cec2fadf9549faea054f1c50713d4dcb5e
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-09-18 23:04:18 +0530

    tweet verify

--- routes/verify.js
+++ routes/verify.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const Twitter = require('twitter')
+const User = require('../models/User')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.redirect('/login')
+const twitter = new Twitter({
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+
+router.post('/tweet', async(req, res) => {
+    let user = await User.findOne({ access_token: req.body.access_token })
+    if (user) {
+        twitter.get(`statuses/show/${req.body.tweetLink.split('/')[req.body.tweetLink.length - 1]}`, (err, tweet, response) => {
+            let today = new Date()
+            var yesterday = new Date(today.getTime());
+            yesterday.setDate(today.getDate() - 1);
+            let date = new Date(tweet.created_at)
+            if (tweet.text.includes(req.body.localBusiness) && tweet.user.id_str === user.connections.twitter.userId && date > yesterday) {
+                res.json({ success: true })
+            }
+        })
+    } else {
+        res.json({ success: false, message: 'User not found' })
+    }
 });
 
 module.exports = router
